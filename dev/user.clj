3c66cfeb2566f59237ec80fff8a1afe1e18d33c6
Gregg Reynolds <reynolds-gregg@norc.org>
2015-09-05 23:38:15 -0500

    misc edits

--- dev/user.clj
+++ dev/user.clj
@@ -3,59 +3,47 @@
 
 ;; (println "hello, user")
 
+(require '[clojure.test :refer [run-tests test-var]])
+(require '[clojure.tools.namespace.repl :refer [refresh refresh-all]])
+
+(defn rt
+  [t]
+  (clojure.test/test-var t))
+
+;;(require 'test.ctor-local :reload-all)
+;;(run-tests 'test.ctor-local)
+
 (import '(com.google.appengine.tools.development.testing
           LocalServiceTestHelper
           LocalServiceTestConfig
           LocalDatastoreServiceTestConfig))
 
-(require '(migae [datastore :as ds]))
-(require '(migae.datastore [keychain :as k]))
+;;(require '(migae [datastore :as ds]))
+;;(require '(migae.datastore [keychain :as k]))
 
 ;; LocalServiceTestHelper
 ;; static void	endRequest()
-;;     Indicate the request has ended so that local services can do any post request work.
 ;; static ApiProxyLocal	getApiProxyLocal()
-;;     Convenience function for getting ahold of the currently registered ApiProxyLocal.
 ;; static LocalRpcService	getLocalService(java.lang.String serviceName)
-;;     Convenience function for getting ahold of a specific local service.
 ;; protected com.google.apphosting.api.ApiProxy.Environment	newEnvironment()
-;;     Constructs the ApiProxy.Environment that will be installed.
 ;; protected LocalServerEnvironment	newLocalServerEnvironment()
-;;     Constructs the LocalServerEnvironment that will be installed.
 ;; LocalServiceTestHelper	setClock(Clock clock)
-;;     Sets the clock with which all local services will be initialized.
 ;; LocalServiceTestHelper	setEnforceApiDeadlines(boolean val)
-;;     Determines whether or not API calls should be subject to the same deadlines as in production.
 ;; LocalServiceTestHelper	setEnvAppId(java.lang.String envAppId)
-;;     The value to be returned by ApiProxy.getCurrentEnvironment().getAppId()
 ;; LocalServiceTestHelper	setEnvAttributes(java.util.Map<java.lang.String,java.lang.Object> envAttributes)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getAttributes()
 ;; LocalServiceTestHelper	setEnvAuthDomain(java.lang.String envAuthDomain)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getAuthDomain()
 ;; LocalServiceTestHelper	setEnvEmail(java.lang.String envEmail)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getEmail()
 ;; LocalServiceTestHelper	setEnvInstance(java.lang.String envInstance)
-;; The current instance id held by ApiProxy.getCurrentEnvironment()
 ;; LocalServiceTestHelper	setEnvIsAdmin(boolean envIsAdmin)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().isAdmin()
 ;; LocalServiceTestHelper	setEnvIsLoggedIn(boolean envIsLoggedIn)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().isLoggedIn()
 ;; LocalServiceTestHelper	setEnvModuleId(java.lang.String envModuleId)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getModuleId()
 ;; LocalServiceTestHelper	setEnvRequestNamespace(java.lang.String envRequestNamespace)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getRequestNamespace()
 ;; LocalServiceTestHelper	setEnvVersionId(java.lang.String envVersionId)
-;; The value to be returned by ApiProxy.getCurrentEnvironment().getVersionId()
 ;; cLocalServiceTestHelper	setRemainingMillisTimer(LocalServiceTestHelper.RequestMillisTimer timer)
-;; Sets the object that will return the value to be returned by ApiProxy.getCurrentEnvironment().getRemainingMillis()
 ;; LocalServiceTestHelper	setSimulateProdLatencies(boolean val)
-;; Determines whether or not local services should simulate production latencies.
 ;; LocalServiceTestHelper	setTimeZone(java.util.TimeZone timeZone)
-;; Sets the time zone in which tests will execute.
 ;; LocalServiceTestHelper	setUp()
-;; Set up an environment in which tests that use local services can execute.
 ;; void	tearDown()
-;; Tear down the environment in which tests that use local services can execute.
 
 (def ds-test-env (.setEnforceApiDeadlines
                   (LocalServiceTestHelper.
@@ -72,7 +60,7 @@
 (def m {:a 1})
 
 (.setUp ds-test-env)
-(def em (ds/entity-map k m))
+;; (def em (ds/entity-map k m))
 
 ;; (.tearDown ds-test-env))))
 
