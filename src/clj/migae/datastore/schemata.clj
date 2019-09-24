890f0e5b491b8485325e0532b7217a2a91cfa03f
Gregg Reynolds <reynolds-gregg@norc.org>
2019-09-23 20:44:15 -0500

    schemata changes

--- src/clj/migae/datastore/schemata.clj
+++ src/clj/migae/datastore/schemata.clj
@@ -1,21 +1,30 @@
 (ns migae.datastore.schemata
   (:require [clojure.tools.logging :as log :only [debug info]]
             ;; [clojure.tools.reader.edn :as edn]
-            [schema.core :as s])) ;; :include-macros true]
+            ;; [schema.core :as s]
+            )) ;; :include-macros true]
 
 (clojure.core/println "loading migae.datastore.schemata")
 
 ;;  keys:  :Schema/#version-number, e.g. :Persons/#1
 (defonce schemata (atom {}))
 
+(defn register-schema
+  [k schema]
+  ;; FIXME: validation
+  (if (and (vector? k)
+           (not (empty? k))
+           (every?
+            #(and (keyword? %) (not (nil? (namespace %))))
+           k))
+    (swap! schemata assoc k schema)
+    (throw (IllegalArgumentException. (str "First arg must be keyword")))))
+
+(defn dump-schemata
+  []
+  (log/debug "schemata: " @schemata))
+
 (defn schema
   [kw]
   (@schemata kw))
 
-(defn register-schema
-  [kw schema]
-  (log/debug "register-schema: " kw schema)
-  ;; FIXME: validation
-  (swap! schemata assoc kw schema))
-
-(defn dump [] (log/debug "schemata: " @schemata))
